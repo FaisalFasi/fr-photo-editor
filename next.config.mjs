29871402d254ae73285dfb32d371547c9fc11f38
faisalrehman <faisalfasi18@gmail.com>
2024-06-09 14:19:15 +0200

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -9,20 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self' flowing-rattler-60.accounts.dev flowing-rattler-60.clerk.accounts.dev cdn.jsdelivr.net js.sentry-cdn.com browser.sentry-cdn.com *.ingest.sentry.io challenges.cloudflare.com scdn.clerk.com segapi.clerk.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://clerk.accounts.dev",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
