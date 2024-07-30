cb62131e844b2a4f029d58e6ed42d3d725869ebf
faisalrehman <faisalfasi18@gmail.com>
2024-07-30 13:19:04 +0200

    updated clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,28 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// const isProtectedRoute = createRouteMatcher(["/profile", "/settings"]);
+// what does (.*) do?
+// it matches any character (.) zero or more times (*) using the wildcard character (*) in regex syntax,
+// so it will match /home, /home/1, /home/2, /home/3, etc.
+// and /dashboard, /dashboard/1, /dashboard/2, /dashboard/3, etc.
 const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/forum(.*)",
   "/profile",
   "/settings",
-  // what does (.*) do?
-  // it matches any character (.) zero or more times (*) using the wildcard character (*) in regex syntax,
-  // so it will match /home, /home/1, /home/2, /home/3, etc.
-  // and /dashboard, /dashboard/1, /dashboard/2, /dashboard/3, etc.
 ]);
 
-const isPublicRoute = createRouteMatcher(["/api/webhooks/clerk"]);
+// const isPublicRoute = createRouteMatcher(["/api/webhooks/clerk"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) {
-    return;
-  }
+  // if (isPublicRoute(req)) {
+  //   return;
+  // }
+  if (isProtectedRoute(req)) auth().protect();
 
-  if (!auth().userId && isProtectedRoute(req)) {
-    return auth().redirectToSignIn();
-  }
+  // if (!auth().userId && isProtectedRoute(req)) {
+  //   return auth().redirectToSignIn();
+  // }
 });
 
 export const config = {
