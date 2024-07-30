5b541f31bd03f3978259f691093bfbc6815bfd76
faisalrehman <faisalfasi18@gmail.com>
2024-07-30 13:42:13 +0200

    refactor: Update clerk middleware to handle public routes and protected routes separately

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,24 +5,22 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 // it matches any character (.) zero or more times (*) using the wildcard character (*) in regex syntax,
 // so it will match /home, /home/1, /home/2, /home/3, etc.
 // and /dashboard, /dashboard/1, /dashboard/2, /dashboard/3, etc.
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/forum(.*)",
-  "/profile",
-  "/settings",
+const isProtectedRoute = createRouteMatcher(["/profile", "/settings"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhooks/clerk",
+  "/api/webhooks/stripe",
 ]);
 
-// const isPublicRoute = createRouteMatcher(["/api/webhooks/clerk"]);
-
 export default clerkMiddleware((auth, req) => {
-  // if (isPublicRoute(req)) {
-  //   return;
-  // }
-  if (isProtectedRoute(req)) auth().protect();
+  if (isPublicRoute(req)) {
+    return;
+  }
+  // if (isProtectedRoute(req)) auth().protect();
 
-  // if (!auth().userId && isProtectedRoute(req)) {
-  //   return auth().redirectToSignIn();
-  // }
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn();
+  }
 });
 
 export const config = {
