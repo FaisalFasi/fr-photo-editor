dac7bfd35063a018bae89f618a4a6da88b933734
faisalrehman <faisalfasi18@gmail.com>
2024-08-01 18:31:28 +0200

    refactor: Update credit prices and fees

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -71,11 +71,11 @@ export const plans = [
     _id: 2,
     name: "Pro Package",
     icon: "/assets/icons/free-plan.svg",
-    price: 40,
-    credits: 120,
+    price: 5,
+    credits: 50,
     inclusions: [
       {
-        label: "120 Credits",
+        label: "50 Credits",
         isIncluded: true,
       },
       {
@@ -96,11 +96,11 @@ export const plans = [
     _id: 3,
     name: "Premium Package",
     icon: "/assets/icons/free-plan.svg",
-    price: 199,
-    credits: 2000,
+    price: 14.99,
+    credits: 200,
     inclusions: [
       {
-        label: "2000 Credits",
+        label: "200 Credits",
         isIncluded: true,
       },
       {
@@ -190,4 +190,4 @@ export const defaultValues = {
   publicId: "",
 };
 
-export const creditFee = -1;
+export const creditFee = -10;
--- src/lib/actions/transaction.action.ts
+++ src/lib/actions/transaction.action.ts
@@ -1,18 +1,18 @@
 "use server";
 
-import Stripe from "stripe";
 import { redirect } from "next/navigation";
-import Transaction from "../models/transaction.model";
-import { updateCredits } from "./user.actions";
+import Stripe from "stripe";
 import { handleError } from "../utils";
+import { connectToDatabase } from "../database/mongoose";
+import { updateCredits } from "./user.actions";
+import Transaction from "../models/transaction.model";
 
-// checkoutCredits is to checkout credits
 export async function checkoutCredits(transaction: CheckoutTransactionParams) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
   const amount = Number(transaction.amount) * 100;
+
   const session = await stripe.checkout.sessions.create({
-    //  line_items is to create a line item for the transaction with the price data and product data
     line_items: [
       {
         price_data: {
@@ -38,11 +38,12 @@ export async function checkoutCredits(transaction: CheckoutTransactionParams) {
   redirect(session.url!);
 }
 
-// create a transaction
 export async function createTransaction(transaction: CreateTransactionParams) {
-  // create a transaction
-
   try {
+    console.log("connecting to database for transaction");
+    await connectToDatabase();
+    console.log(transaction);
+    // Create a new transaction with a buyerId
     const newTransaction = await Transaction.create({
       ...transaction,
       buyer: transaction.buyerId,
@@ -55,3 +56,64 @@ export async function createTransaction(transaction: CreateTransactionParams) {
     handleError(error);
   }
 }
+
+// "use server";
+
+// import Stripe from "stripe";
+// import { redirect } from "next/navigation";
+// import Transaction from "../models/transaction.model";
+// import { updateCredits } from "./user.actions";
+// import { handleError } from "../utils";
+// import { connectToDatabase } from "../database/mongoose";
+
+// // checkoutCredits is to checkout credits
+// export async function checkoutCredits(transaction: CheckoutTransactionParams) {
+//   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+//   const amount = Number(transaction.amount) * 100;
+//   const session = await stripe.checkout.sessions.create({
+//     //  line_items is to create a line item for the transaction with the price data and product data
+//     line_items: [
+//       {
+//         price_data: {
+//           currency: "usd",
+//           unit_amount: amount,
+//           product_data: {
+//             name: transaction.plan,
+//           },
+//         },
+//         quantity: 1,
+//       },
+//     ],
+//     metadata: {
+//       plan: transaction.plan,
+//       credits: transaction.credits,
+//       buyerId: transaction.buyerId,
+//     },
+//     mode: "payment",
+//     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+//     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+//   });
+
+//   redirect(session.url!);
+// }
+
+// // create a transaction
+// export async function createTransaction(transaction: CreateTransactionParams) {
+//   // create a transaction
+
+//   await connectToDatabase();
+
+//   try {
+//     const newTransaction = await Transaction.create({
+//       ...transaction,
+//       buyer: transaction.buyerId,
+//     });
+
+//     await updateCredits(transaction.buyerId, transaction.credits);
+
+//     return JSON.parse(JSON.stringify(newTransaction));
+//   } catch (error) {
+//     handleError(error);
+//   }
+// }
